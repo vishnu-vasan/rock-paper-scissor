8ebd87e48358dd40815b4ee011548d48a730b5d4
Vishnu <vasan2001@gmail.com>
2020-11-30 11:51:21 +0530

    added image buttons and alert

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Button,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 
 export default function App() {
+  const sample_alert = () => {
+    alert("Tapped the button");
+  };
   return (
     <View>
-    <View style={styles.container}>
-      <Button title="Rock"/>
-      <Button title="Paper"/>
-      <Button title="Scissor"/>
-    </View>
-    <View style={styles.container1}>
-      <Button title="Rock"/>
-      <Button title="Paper"/>
-      <Button title="Scissor"/>
-    </View>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={sample_alert}>
+          <Image source={require("./assets/rock.png")} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={sample_alert}>
+          <Image source={require("./assets/paper.png")} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={sample_alert}>
+          <Image source={require("./assets/scissors.png")} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container1}>
+        <TouchableOpacity onPress={sample_alert}>
+          <Image source={require("./assets/rock.png")} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={sample_alert}>
+          <Image source={require("./assets/paper.png")} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={sample_alert}>
+          <Image source={require("./assets/scissors.png")} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -22,15 +43,15 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: 'space-around',
+    justifyContent: "space-around",
     height: 200,
-    borderColor: 'black',
+    borderColor: "black",
   },
   container1: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: 'space-around',
-    height: 1200,
-    borderColor: 'black',
-  }
+    justifyContent: "space-around",
+    height: 1100,
+    borderColor: "black",
+  },
 });
